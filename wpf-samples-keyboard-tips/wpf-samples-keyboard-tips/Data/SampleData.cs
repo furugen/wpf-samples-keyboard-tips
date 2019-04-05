f31052bf2e632bf57992ffdd856225840ccab1e8
Yuu Fuerugen <furugen098@gmail.com>
2019-04-05 10:39:12 +0900

    サンプルコミット

---
+++ wpf-samples-keyboard-tips/wpf-samples-keyboard-tips/Data/SampleData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_samples_keyboard_tips.Data
+{
+    public class SampleData
+    {
+        public string Name { get; set; }
+        public string Hiragana { get; set; }
+        public string Romaji { get; set; }
+    }
+}
